dfd644300f9e2cf442afe79721368289e11aac9b
Héctor Romero López <hector@hromerol.es>
2022-12-27 13:59:52 +0100

    ADD: First version of relational database

--- estructure.sql
+++ estructure.sql
@@ -1,5 +1,15 @@
+DROP TABLE registro;
+DROP TABLE DNS;
+drop table contenedorServidor;
+drop table aplicacionServidor;
+DROP TABLE servidor;
+DROP TABLE aplicacion;
+DROP TABLE contenedor;
+DROP TABLE aplicacion;
+
+
 CREATE TABLE DNS(
-    nombre varchar(12) not null,
+    nombre varchar(12) primary key,
     expirar_date DATE,
     protegido boolean
 );
@@ -14,3 +24,41 @@ CREATE TABLE registro (
     constraint dns FOREIGN KEY (dns) references dns(nombre),
     constraint dnstype check (tipo in ('A','AAAA','CAA','CNAME','MX','NS','SRV','TXT'))
 );
+
+CREATE TABLE servidor (
+
+    nombre varchar(12) primary key,
+    ipv4 varchar(12) not null
+);
+
+CREATE TABLE contenedor (
+
+    id varchar(64) primary key,
+    nombre varchar(20) not null,
+    estado varchar(10),
+    constraint estadotype check (estado in ('running','stopped','restarting','aborted'))
+);
+
+CREATE TABLE contenedorServidor(
+    servidor_nombre varchar(12),
+    contenedor_id varchar(64),
+    constraint servidor foreign KEY (servidor_nombre) references servidor(nombre),
+    constraint contenedor foreign key (contenedor_id) references contenedor(id)
+    
+);
+
+CREATE TABLE aplicacion (
+
+    nombre varchar(12) primary key,
+    descripcion varchar(200),
+    tipo varchar(12),
+    playbook BFILE
+);
+
+CREATE TABLE aplicacionServidor(
+    aplicacionNombre varchar(12),
+    servidorNombre varchar(12),
+    constraint servidorAplicacionFK foreign key (servidorNombre) references servidor(nombre),
+    constraint aplicacionServidorFK foreign key (aplicacionNombre) references aplicacion(nombre)
+);
+
